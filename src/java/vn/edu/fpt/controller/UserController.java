4a4e7a33c9fa6a680ba31cc78ffbd36a84bac307
rinauwu2004 <daivdhe181300@fpt.edu.vn>
2025-04-15 09:07:17 +0700

    Initial project

---
+++ src/java/vn/edu/fpt/controller/UserController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.edu.fpt.controller;
+
+/**
+ *
+ * @author Rinaaaa
+ */
+public class UserController {
+    public void saveUser() {
+        
+    }
+    
+    public void editUser(){
+        
+    }
+    
+    public void getUser(){
+        
+    }
+    
+    public void listUsers(){
+        
+    }
+    
+    public void deleteUser(){
+        
+    }
+}
---
+++ src/java/vn/edu/fpt/dao/UserDao.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vn.edu.fpt.dao;
+
+/**
+ *
+ * @author Rinaaaa
+ */
+public class UserDao {
+    public void saveUser() {
+        
+    }
+    
+    public void editUser(){
+        
+    }
+    
+    public void getUser(){
+        
+    }
+    
+    public void listUsers(){
+        
+    }
+    
+    public void deleteUser(){
+        
+    }
+}
