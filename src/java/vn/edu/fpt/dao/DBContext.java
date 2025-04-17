e243414fd57ca04558ce0e3112d4bbc7f301ad81
rinauwu2004 <daivdhe181300@fpt.edu.vn>
2025-04-17 11:24:19 +0700

    Create DBContext for database connection

--- src/java/vn/edu/fpt/dao/DBContext.java
+++ src/java/vn/edu/fpt/dao/DBContext.java
@@ -4,10 +4,29 @@
  */
 package vn.edu.fpt.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Rinaaaa
  */
 public class DBContext {
-    
+
+    protected static Connection connection;
+
+    public DBContext() {
+        try {
+            String user = "rina";
+            String pass = "26122004";
+            String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=SWP391_M2_BL5;trustServerCertificate=true";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
